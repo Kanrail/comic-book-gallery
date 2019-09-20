0c07e94efc20a7e31638c215f4705db4b618a02f
Daniel Bowen <daniel.lawrence.bowen@gmail.com>
2019-09-20 14:51:43 -0500

    Fully defined bridge entity, refactored code to incorporate.

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -11,7 +11,7 @@ namespace ComicBookGalleryModel.Models
     {
         public ComicBook()
         {
-            Artists = new List<Artist>();
+            Artists = new List<ComicBookArtist>();
         }
 
         // ID, ComicBookId, ComicBookID
@@ -23,11 +23,20 @@ namespace ComicBookGalleryModel.Models
         public decimal? AverageRating { get; set; }
 
         public Series Series { get; set; }
-        public ICollection<Artist> Artists { get; set; }
+        public ICollection<ComicBookArtist> Artists { get; set; }
 
         public string DisplayText
         {
             get { return $"{Series?.Title} #{IssueNumber}"; }
+        }
+
+        public void AddArtist(Artist artist, Role role)
+        {
+            Artists.Add(new ComicBookArtist()
+                {
+                    Artist = artist,
+                    Role = role
+                });
         }
     }
 }
